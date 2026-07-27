b10608ab3a215cf013ef8d1575edca24353bf356
Jude Andy Convencido <jconvencido@fullscale.ph>
2026-07-27 10:30:07 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "../components/analytics/GoogleAnalytics";
 import { BFCACHE_RECOVERY_SCRIPT } from "../lib/bfcache/recovery-script";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body className={inter.variable}>
         <script dangerouslySetInnerHTML={{ __html: BFCACHE_RECOVERY_SCRIPT }} />
+        <GoogleAnalytics />
         {children}
         <SpeedInsights />
       </body>
